9953ab02be54b51dd3e503db8a8f5cc459c8825c
jg <JohnGavin@users.noreply.github.com>
2023-06-26 20:10:31 +0000

    greata with working example

---
+++ brms/greta/01_greta_em.R
@@ -0,0 +1,12 @@
+# https://blog.martinez.fyi/post/nvidia-docker-greta/
+library(greta)
+x <- iris$Petal.Length
+y <- iris$Sepal.Length
+int <- normal(0, 5)
+coef <- normal(0, 3)
+sd <- lognormal(0, 3)
+mean <- int + coef * x
+distribution(y) <- normal(mean, sd)
+m <- model(int, coef, sd)
+draws <- mcmc(m, n_samples = 1000)
+summary(draws)   
